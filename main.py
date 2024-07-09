c418a161344fbb336fe98a0d8e423e3f48726872
Mikael Cauã <mikaelcauasilva1@gmail.com>
2024-07-09 13:21:28 -0300

    add post method

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from flask import Flask
+# from flask import Flask,request
+# from flask_cors import CORS
+# from model.tabela import Tabela
+
+
+# app = Flask(__name__)
+# CORS(app)  # Habilita CORS para todo o aplicativo
+
+# tabela = Tabela()
+
+# @app.route('/',methods=['GET', 'POST'])
+# def homepage():
+#     if request.method == 'GET':
+#         dados = tabela.obter_dados_como_json()
+#         return dados
+#     elif request.method == 'POST':
+#         tabela.postPrestacao
+
+# if __name__ == "__main__":
+#     app.run()
+
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model.tabela import Tabela
-
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS para todo o aplicativo
 
-arquivo_csv = './base_dados/prestacao_contas.csv'
 tabela = Tabela()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    dados = tabela.obter_dados_como_json()
-    return dados
+    if request.method == 'GET':
+        return tabela.obter_dados_como_json()
+    elif request.method == 'POST':
+        return tabela.postPrestacao(request.get_json())
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- model/tabela.py
+++ model/tabela.py
@@ -1,3 +1,113 @@
+
+# #instalar
+# #pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+# import os.path
+# from flask import jsonify,request
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
+
+# # If modifying these scopes, delete the file 
+# SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# SAMPLE_SPREADSHEET_ID = "1-HDlz48aobDDazgdyoZW0LxVU_8ztUik68Bkk786kqA"
+# SAMPLE_RANGE_NAME = "Página1!A2:J100"
+
+# class Tabela:
+#     def __init__(self):
+#         self.dados = None
+#         self.carregar_dados()
+
+#     def carregar_dados(self):
+#         creds = None
+#         if os.path.exists("./auth/token.json"):
+#             creds = Credentials.from_authorized_user_file("./auth/token.json", SCOPES)
+#         if not creds or not creds.valid:
+#             if creds and creds.expired and creds.refresh_token:
+#                 creds.refresh(Request())
+#             else:
+#                 flow = InstalledAppFlow.from_client_secrets_file(
+#                     "./auth/credentials.json", SCOPES
+#                 )
+#                 creds = flow.run_local_server(port=0)
+#             # Save the credentials for the next run
+#             with open("./auth/token.json", "w") as token:
+#                 token.write(creds.to_json())
+
+#         try:
+#             service = build('sheets', 'v4', credentials=creds,client_options={"quota_project_id": SAMPLE_SPREADSHEET_ID})
+
+#             # Call the Sheets API
+#             sheet = service.spreadsheets()
+#             result = (
+#                 sheet.values()
+#                 .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+#                 .execute()
+#             )
+#             values = result.get("values", [])
+#             for lin in values:
+#                 print(lin)
+            
+#             self.dados = values
+#             # next_free_position = len(values)+1
+#             # valores_adicionar = [
+#             #   ['Teste','R$ 10.000']
+#             # ]
+#             # result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,range=f'A{next_free_position}',valueInputOption="USER_ENTERED",body={'values':valores_adicionar}).execute()
+#         except HttpError as err:
+#             print(err)
+
+#     def obter_dados_como_json(self):
+#         prestacoes_contas = []
+#         if self.dados is not None:
+#             for index, linha in enumerate(self.dados):
+#                 dados_linha = {
+#                     'descricao': str(linha[0]),
+#                     'instituicao': str(linha[1]),
+#                     'cpf': str(linha[2]),
+#                     'tipoPagamento': str(linha[3]),
+#                     'dataInicial': str(linha[4]),
+#                     'formaPagamento': str(linha[5]),
+#                     'dataFinal': str(linha[6]),
+#                     'valor': str(linha[7]),
+#                     'mes': str(linha[8]),
+#                     'ano': str(linha[9]),
+#                     'id': index
+#                 }
+#                 prestacoes_contas.append(dados_linha)
+#         return jsonify(prestacoes_contas)
+    
+#     def postPrestacao():
+#             data = request.get_json()  # Obtem os dados JSON da requisição
+#             descricao = data.get('descricao')  # Extrai o campo 'descricao' dos dados
+            
+#             # Obter a próxima posição livre na planilha
+#             sheet = service.spreadsheets()
+#             result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+#                                         range=SAMPLE_RANGE_NAME).execute()
+#             values = result.get('values', [])
+#             next_free_position = len(values) + 1
+
+#             # Dados a adicionar
+#             valores_adicionar = [
+#                 [descricao, 'R$ 10.000']  # Substitua pelo valor que deseja adicionar
+#             ]
+
+#             # Atualizar a planilha
+#             result = sheet.values().update(
+#                 spreadsheetId=SAMPLE_SPREADSHEET_ID,
+#                 range=f'A{next_free_position}',
+#                 valueInputOption="USER_ENTERED",
+#                 body={'values': valores_adicionar}
+#             ).execute()
+
+#             # Resposta após processamento
+#             response = {'message': 'Data received and added to spreadsheet successfully', 'descricao': descricao}
+#             return jsonify(response), 200
+
+
 import os.path
 from flask import jsonify
 from google.auth.transport.requests import Request
@@ -6,7 +116,6 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 SAMPLE_SPREADSHEET_ID = "1-HDlz48aobDDazgdyoZW0LxVU_8ztUik68Bkk786kqA"
@@ -29,30 +138,14 @@ class Tabela:
                     "./auth/credentials.json", SCOPES
                 )
                 creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
             with open("./auth/token.json", "w") as token:
                 token.write(creds.to_json())
 
         try:
-            service = build('sheets', 'v4', credentials=creds,client_options={"quota_project_id": SAMPLE_SPREADSHEET_ID})
-
-            # Call the Sheets API
+            service = build('sheets', 'v4', credentials=creds, client_options={"quota_project_id": SAMPLE_SPREADSHEET_ID})
             sheet = service.spreadsheets()
-            result = (
-                sheet.values()
-                .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-                .execute()
-            )
-            values = result.get("values", [])
-            for lin in values:
-                print(lin)
-            
-            self.dados = values
-            # next_free_position = len(values)+1
-            # valores_adicionar = [
-            #   ['Teste','R$ 10.000']
-            # ]
-            # result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,range=f'A{next_free_position}',valueInputOption="USER_ENTERED",body={'values':valores_adicionar}).execute()
+            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+            self.dados = result.get("values", [])
         except HttpError as err:
             print(err)
 
@@ -76,3 +169,27 @@ class Tabela:
                 prestacoes_contas.append(dados_linha)
         return jsonify(prestacoes_contas)
 
+    def postPrestacao(self, data):
+        descricao = data.get('descricao')  # Extrai o campo 'descricao' dos dados
+        
+        creds = Credentials.from_authorized_user_file("./auth/token.json", SCOPES)
+        service = build('sheets', 'v4', credentials=creds, client_options={"quota_project_id": SAMPLE_SPREADSHEET_ID})
+        
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+        next_free_position = len(values) + 1
+
+        valores_adicionar = [
+            [descricao, 'R$ 10.000']  # Substitua pelo valor que deseja adicionar
+        ]
+
+        result = sheet.values().update(
+            spreadsheetId=SAMPLE_SPREADSHEET_ID,
+            range=f'A{next_free_position}',
+            valueInputOption="USER_ENTERED",
+            body={'values': valores_adicionar}
+        ).execute()
+
+        response = {'message': 'Data received and added to spreadsheet successfully', 'descricao': descricao}
+        return jsonify(response), 200
