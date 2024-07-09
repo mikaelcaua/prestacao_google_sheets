8754a14f60ab21d6b5a519876753de69278d13a1
Mikael Cauã <mikaelcauasilva1@gmail.com>
2024-07-09 16:45:50 -0300

    add form fields

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-# from flask import Flask,request
-# from flask_cors import CORS
-# from model.tabela import Tabela
-
-
-# app = Flask(__name__)
-# CORS(app)  # Habilita CORS para todo o aplicativo
-
-# tabela = Tabela()
-
-# @app.route('/',methods=['GET', 'POST'])
-# def homepage():
-#     if request.method == 'GET':
-#         dados = tabela.obter_dados_como_json()
-#         return dados
-#     elif request.method == 'POST':
-#         tabela.postPrestacao
-
-# if __name__ == "__main__":
-#     app.run()
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model.tabela import Tabela
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para todo o aplicativo
+CORS(app)
 
 tabela = Tabela()
 
